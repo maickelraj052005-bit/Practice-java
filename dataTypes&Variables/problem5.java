ad98abc727d579d95032b7791dc4eb4e22454bcf
maickelraj S <maickelraj052005@gmail.com>
2026-05-24 10:07:17 +0530

    Add files via upload

---
+++ dataTypes&Variables/problem5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println(name);
+        sc.close();
+    }
+}
