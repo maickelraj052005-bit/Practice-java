c6c49947b214d04c59985132a85a22372c13b11d
maickelraj S <maickelraj052005@gmail.com>
2026-05-27 15:24:23 +0530

    Add files via upload

---
+++ dataTypes&Variables/problem6.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class problem6 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+         byte age = sc.nextByte();
+
+         System.out.println(age);
+    }
+}
