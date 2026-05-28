fd93893f272301deee8a8107c7e97736862a83f7
maickelraj S <maickelraj052005@gmail.com>
2026-05-28 20:26:20 +0530

    Add files via upload

---
+++ dataTypes&Variables/problem8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        float cgpa = sc.nextFloat();
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(cgpa);
+
+        sc.close();
+    }
+}
