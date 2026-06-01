18070573402fb10fa7dc5330d93a629b9e4c0f13
maickelraj S <maickelraj052005@gmail.com>
2026-06-01 21:52:27 +0530

    Add files via upload

---
+++ dataTypes&Variables/problem11.java
@@ -0,0 +1,16 @@
+import java.util.*;;
+public class problem11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        float mark = sc.nextFloat();
+
+
+        System.out.println("Name :" +name);
+        System.out.println("Age :" +age);
+        System.out.println("Mark :" +mark);
+
+        sc.close();
+    }
+}
