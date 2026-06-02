0c97b5063529f464cc7ec9d7e11f098981eab5ee
maickelraj S <maickelraj052005@gmail.com>
2026-06-02 21:41:59 +0530

    Add files via upload

---
+++ dataTypes&Variables/problem12.java
@@ -0,0 +1,13 @@
+import java.util.*;;
+public class problem12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+        int width = sc.nextInt();
+
+        System.out.print("Area of Rectangle : "+ length*width);
+
+        sc.close();
+    }
+}
