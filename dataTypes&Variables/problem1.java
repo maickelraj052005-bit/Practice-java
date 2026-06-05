8e5541d36bc7a32620675d1c1139ba4448d393f2
maickelraj S <maickelraj052005@gmail.com>
2026-06-05 22:53:49 +0530

    Fix formatting and add newline at end of file

--- dataTypes&Variables/problem1.java
+++ dataTypes&Variables/problem1.java
@@ -3,7 +3,7 @@ public class problem1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+ 
         int num = sc.nextInt();
         System.out.println(num);
         sc.close();
