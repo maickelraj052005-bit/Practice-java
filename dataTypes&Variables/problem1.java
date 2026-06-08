2437245d7a7fcc7eef54c3f39ee29312b8c9ae7a
maickelraj S <maickelraj052005@gmail.com>
2026-06-08 19:49:07 +0530

    Update problem1.java

--- dataTypes&Variables/problem1.java
+++ dataTypes&Variables/problem1.java
@@ -4,7 +4,7 @@ public class problem1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
  
-        int num = sc.nextInt();
+        int num = sc.nextInt(); 
         System.out.println(num);
         sc.close();
 
