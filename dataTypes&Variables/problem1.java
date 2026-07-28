9f5158ee3b5b9b8d0c63f3cc7f04368b21eedbba
maickelraj S <maickelraj052005@gmail.com>
2026-07-28 16:30:41 +0530

    Format code in problem1.java

--- dataTypes&Variables/problem1.java
+++ dataTypes&Variables/problem1.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class problem1 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
  
         int num = sc.nextInt(); 
         System.out.println(num);  
